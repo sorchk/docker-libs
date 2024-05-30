bc94786358abb45d7ed5baecae3fcc9ce6b1a0a4
sorc <sorc@qq.com>
2024-05-30 14:44:14 +0800

    修复jre安装版本错误

--- libreoffice/build.sh
+++ libreoffice/build.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 name=libreoffice
 ver=$1
+ubuntu_ver=22.04
+jdk_ver=openjdk-8-jre
 build_date=`date +"%Y%m%d"`
 if [ -z "${ver}" ] ;then
-  ver=jre8
+  ver=7.3
 fi
 echo ${ver}_${build_date}
 # export DOCKER_CLI_EXPERIMENTAL=enabled
@@ -13,9 +15,12 @@ echo ${DOCKER_HUB_KEY} | docker login --username ${DOCKER_HUB_USER} --password-s
 docker buildx build \
   --platform linux/arm64,linux/amd64 \
   --build-arg VER=${ver} \
+  --build-arg UBUNTU_VER=${ubuntu_ver} \
+  --build-arg JDK_VER=${jdk_ver} \
   --build-arg BUILD_DATE=${build_date} \
   --push \
-  --tag sorc/${name}:${ver}_${build_date} \
+  --tag sorc/${name}:${ver}_${jdk_ver}_${build_date} \
+  --tag sorc/${name}:${ver}_${jdk_ver} \
   --tag sorc/${name}:${ver} \
   --tag sorc/${name}:latest .
 docker logout
--- libreoffice/test.sh
+++ libreoffice/test.sh
@@ -1,4 +1,19 @@
 
 docker run --rm -it \
 -e "TZ=Asia/Shanghai" \
-sorc/libreoffice:latest /bin/bash
+sorc/libreoffice:latest
+
+
+docker run --rm -it ubuntu:20.04 /bin/bash
+docker run --rm -it ubuntu:22.04 /bin/bash
+docker run --rm -it ubuntu:24.04 /bin/bash
+apt update && apt search libreoffice |grep libreoffice/
+
+
+docker  build \
+  -f Dockerfile.test \
+  --build-arg VER=7.3 \
+  --build-arg UBUNTU_VER=22.04 \
+  --build-arg JDK_VER=openjdk-8-jre \
+  --output=type=docker \
+  --tag sorc/test:latest .
