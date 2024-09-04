f296c9c4697e1488b726c0e9e5c37882727ec8ca
sorc <sorc@qq.com>
2024-09-04 09:21:52 +0800

    添加sysbeanch  mysql数据库压力测试工具

---
+++ sysbench/docker-build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+name=sysbench
+ver=$1
+build_date=`date +"%Y%m%d"`
+if [ -z "${ver}" ] ;then
+  ver=1.0.20
+fi
+echo ${ver}_${build_date}
+export DOCKER_CLI_EXPERIMENTAL=enabled
+echo ${DOCKER_HUB_KEY} | docker login --username ${DOCKER_HUB_USER} --password-stdin
+docker buildx build \
+  --platform linux/amd64,linux/arm64 \
+  --build-arg VER=${ver} \
+  --build-arg BUILD_DATE=${build_date} \
+  --push \
+  --tag sorc/${name}:${ver}_${build_date} \
+  --tag sorc/${name}:${ver} \
+  --tag sorc/${name}:latest .
+
+
+  # --build-arg HTTP_PROXY=http://10.10.10.41:1083 \
+  # --build-arg HTTPS_PROXY=http://10.10.10.41:1083 \
